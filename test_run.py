e6948515ac56a57f8a8a39c3365c3e2e3e4808b0
NGUYEN MINH KHOA <minhkhoajapan0701@gmail.com>
2024-12-23 13:59:59 +0900

    Filter data where col2 == 11

--- test_run.py
+++ test_run.py
@@ -1,6 +1,7 @@
 import gpin_model
 import eo_model
 import numpy as np
+import random
 
 data_file_path = 'PINBSN24SJun.txt'
 
@@ -12,13 +13,34 @@ data = np.genfromtxt(
     names = ['company_code', 'col2', 'col3', 'date', 'buy', 'sell']
 )
 
-#test run for toyota
-toyota_data = data[data['company_code'] == 7203]
+unique_stock_codes = np.unique(data['company_code'])
+random_stock_codes = random.sample(list(unique_stock_codes), 10)
 
-dates = toyota_data['date']
-buys = toyota_data['buy']
-sells = toyota_data['sell']
+year_range = range(2001, 2024)
+results = {}
 
-toyota_array = np.column_stack((dates, buys, sells))
+for stock_code in random_stock_codes:
+    stock_data = data[(data['company_code'] == stock_code) & (data['col2'] == 11)]
 
-print(gpin_model.fit(buys, sells))
+    if len(stock_data) == 0:
+        print(f"No data for stock code {stock_code}. Skipping")
+        continue
+
+    years = np.array([int(str(d)[:4]) for d in stock_data['date']])
+    results = {}
+
+    for year in year_range:
+        year_mask = years == year
+        yearly_data = stock_data[year_mask]
+
+        buys = yearly_data['buy']
+        sells = yearly_data['sell']
+        results[year] = eo_model.fit(buys, sells)
+    
+    file_name = f"{stock_code}_pin_results.txt"
+    with open(file_name, "w") as file:
+        file.write(f"Results for stock code {stock_code}:\n")
+        for year, pin_result in results.items():
+            file.write(f"{year}: {pin_result}\n")
+    
+    print(f"Results for stock code {stock_code} saved to {file_name}.")
